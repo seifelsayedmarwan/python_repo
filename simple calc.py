4511416215d14d080b5810bb703bf0cfe2be9acb
Elking <seifelsayed68@gmail.com>
2023-05-29 21:04:45 +0300

    add simple calc and Simple calculator operating system update

---
+++ simple calc.py
@@ -0,0 +1,32 @@
+import sys
+#A simple calculator that performs four mathematical operations, 
+# which are addition, subtraction, multiplication and division, 
+# according to the user's choice. It works on two numbers only, according to the user's choice.
+print("Hello")
+num_1 = float(input("Enter the first number: "))
+arithmetic_operation = input("What arithmetic operation do you want?(Just write the sign):  ")
+num_2 = float(input("Enter the second number: "))
+
+def plus(x , y):
+    return x + y
+
+def minus(x , y):
+    return x - y
+
+def multiply(x , y):
+    return x * y
+
+def divide(x , y):
+    return x / y
+
+if arithmetic_operation == "+":
+    print(plus(num_1 , num_2))
+
+elif arithmetic_operation == "-":
+    print(minus(num_1 , num_2))
+
+elif arithmetic_operation == "*":
+    print(multiply(num_1 , num_2))
+    
+elif arithmetic_operation == "/":
+    print(divide(num_1 , num_2))
